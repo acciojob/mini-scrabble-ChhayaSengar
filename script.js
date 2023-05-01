64530eaea7f35738a4c7479bccf94d62d89eba0e
Chhaya Sengar <117908142+ChhayaSengar@users.noreply.github.com>
2023-05-01 13:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 //your code here
-const evaluatedText = document.getElementById('evaluatedText');
-const letterCount = document.getElementById('letterCount');
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
 
-// Add an event listener to the input element for 'input' event
-evaluatedText.addEventListener('input', function() {
-  // Get the current value of the input
-  const text = evaluatedText.value;
-
-  // Update the letter count in the h3 element
-  letterCount.textContent = `${text.length}`;
+evaluatedText.addEventListener('input', () => {
+	let str = evaluatedText.value;
+	letterCount.innerHTML = `${str.length}`;
 });
